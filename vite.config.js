ca60f45c374d98fc5ffcb88c6433faec015567b3
Ahmed Azmain Mahtab <mowama36@gmail.com>
2025-07-28 12:28:58 +0600

    up

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000'
-    }
-  }
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    server: mode === 'development' ? {
+      proxy: {
+        '/api': 'http://localhost:5000'
+      }
+    } : {}
+  };
 });
